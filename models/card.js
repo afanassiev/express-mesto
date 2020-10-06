294a6afd13550b0dbc94a1e8a96a774278dc6a86
Anton Afanasiev <anton.afanasyev93@yandex.ru>
2020-10-06 15:17:43 +0300

    add models and schemas

---
+++ models/card.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /^(https?:\/\/)(www\.)?([\w\W\d]+)(\.)([a-z]{1,10})([\w\W\d]+)?$/.test(v)
+      }
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    likes: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: []
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }
+});
+
+module.exports = mongoose.model('card', cardSchema);
