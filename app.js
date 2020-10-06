09a8eef63c893960663ba369292a3eadd823ede3
Anton Afanasiev <anton.afanasyev93@yandex.ru>
2020-10-06 09:31:59 +0300

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cards = require('./routes/cards');
 const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
 
 app.use(express.static('public'));
 
